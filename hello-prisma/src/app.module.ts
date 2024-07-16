acf0e35be47499f9db60d31ab2632e04231b7276
rxrzzz <temiloluwagithub@gmail.com>
2024-07-16 12:06:39 +0100

    learnt prisma recipe

---
+++ hello-prisma/src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { UserService } from './user/user.service';
+import { PostService } from './post/post.service';
+import { PrismaService } from 'prisma/prisma.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService, UserService, PostService, PrismaService],
+})
+export class AppModule {}
